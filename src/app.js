ef1d9ec61caae9725efb89c775744dce1b63e06c
KarlyMakowski <liropache35@gmail.com>
2022-07-13 19:39:09 +0000

    first steps

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let courtCards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"];
+
+  const generateCard = array => {
+    let randomCard = Math.floor(Math.random() * array.length);
+    return array[randomCard];
+  };
+
+  const resultRandomSuit = generateCard(suits);
+  const right = document.getElementById("rightSuit");
+  const left = document.getElementById("leftSuit");
+  const randomCourt = document.getElementById("courtCard");
+
+  right.innerHTML = resultRandomSuit;
+  left.innerHTML = resultRandomSuit;
+  randomCourt.innerHTML = generateCard(courtCards);
 };
